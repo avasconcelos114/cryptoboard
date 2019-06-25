2919fad9bdae6e9d8dfd4a128f797a9248e1bece
avasconcelos114 <andre.onogoro@gmail.com>
2019-06-25 22:57:28 +0900

    Updates

---
+++ precache-manifest.332dac51486a278387218b23bb5821e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6566dd485af0c23b6a3e853e0eaf2513",
+    "url": "/cryptoboard/index.html"
+  },
+  {
+    "revision": "eff6576204a10aadab02",
+    "url": "/cryptoboard/static/js/2.39eb8182.chunk.js"
+  },
+  {
+    "revision": "b7f1c366054e4d10a285",
+    "url": "/cryptoboard/static/js/main.70e11630.chunk.js"
+  },
+  {
+    "revision": "529113d17c384049eafe",
+    "url": "/cryptoboard/static/js/runtime~main.117c916a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25dfc462746b4ce8bcc768fcbac24770.js"
+  "/cryptoboard/precache-manifest.332dac51486a278387218b23bb5821e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cryptoboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
