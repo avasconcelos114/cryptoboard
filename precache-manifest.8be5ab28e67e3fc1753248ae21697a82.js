60b5d886ef125eb8f72a9830152ccecaa606ecb4
avasconcelos114 <andre.onogoro@gmail.com>
2019-07-14 16:01:56 +0900

    Updates

---
+++ precache-manifest.8be5ab28e67e3fc1753248ae21697a82.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497ef7684cb15f45defb167d9f66a86b",
+    "url": "/cryptoboard/index.html"
+  },
+  {
+    "revision": "cbfb3f9497403f54630e",
+    "url": "/cryptoboard/static/js/2.cb3debe3.chunk.js"
+  },
+  {
+    "revision": "21a8a9d9fe8c6cc34d3d",
+    "url": "/cryptoboard/static/js/main.c03f3bde.chunk.js"
+  },
+  {
+    "revision": "529113d17c384049eafe",
+    "url": "/cryptoboard/static/js/runtime~main.117c916a.js"
+  }
+]);
